a746d326342791bb79c24a17afde13ca40c179a9
Diurno <Diurno@logroniostimessergio.edu>
2024-04-09 08:51:19 +0200

    Clases Curso y Persona Documentadas

--- src/main/java/clases/Curso.java
+++ src/main/java/clases/Curso.java
@@ -7,23 +7,48 @@ package clases;
 import java.util.TreeSet;
 
 /**
- *
- * @author ProfDiurno
+ * Clase que representa un curso.
  */
 public class Curso {
 
     private String nombre;
     private TreeSet<Persona> listaAlumnos;
 
-    protected String getNombre() {
-        return nombre;
-    }
-
+    /**
+     * Constructor de la clase Curso.
+     *
+     * @param nombre El nombre del curso.
+     */
     public Curso(String nombre) {
         this.nombre = nombre;
         listaAlumnos = new TreeSet<>();
     }
 
+    /**
+     * Método para obtener el nombre del curso.
+     *
+     * @return El nombre del curso.
+     */
+    protected String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Método para añadir un alumno al curso.
+     *
+     * @param p El objeto Persona que representa al alumno a añadir.
+     */
+    public void aniadirAlumno(Persona p) {
+        listaAlumnos.add(p);
+    }
+
+    /**
+     * Método que genera una representación en forma de cadena de texto del
+     * curso.
+     *
+     * @return Una cadena de texto que representa el curso y la lista de alumnos
+     * inscritos en él.
+     */
     @Override
     public String toString() {
         String s = "--------------------" + nombre + "-----------------\n";
@@ -33,9 +58,5 @@ public class Curso {
             s += listaAlumno + "\n";
         }
         return s;
-    }
-
-    public void aniadirAlumno(Persona p) {
-        listaAlumnos.add(p);
     }
 }
--- src/main/java/clases/Persona.java
+++ src/main/java/clases/Persona.java
@@ -8,6 +8,9 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.Objects;
 
+/**
+ * Clase que representa una persona.
+ */
 public class Persona implements Comparable<Persona> {
 
     private Nif nif;
@@ -15,6 +18,10 @@ public class Persona implements Comparable<Persona> {
     private char genero;
     private LocalDate nacimiento;
 
+    /**
+     * Constructor por defecto de la clase Persona. Inicializa los valores
+     * predeterminados.
+     */
     public Persona() {
         nif = new Nif();
         nombre = "";
@@ -22,11 +29,26 @@ public class Persona implements Comparable<Persona> {
         nacimiento = LocalDate.of(1990, 1, 1);
     }
 
+    /**
+     * Constructor de la clase Persona con un NIF dado.
+     *
+     * @param nif El número de identificación fiscal (NIF) de la persona.
+     */
     public Persona(int nif) {
         this();
         this.nif = new Nif(nif);
     }
 
+    /**
+     * Constructor de la clase Persona con todos los atributos.
+     *
+     * @param nif El número de identificación fiscal (NIF) de la persona.
+     * @param nombre El nombre de la persona.
+     * @param genero El género de la persona.
+     * @param dia El día de nacimiento de la persona.
+     * @param mes El mes de nacimiento de la persona.
+     * @param ano El año de nacimiento de la persona.
+     */
     public Persona(int nif, String nombre, char genero,
             int dia, int mes, int ano) {
         this.nif = new Nif(nif);
@@ -36,42 +58,94 @@ public class Persona implements Comparable<Persona> {
                 = LocalDate.of(ano, mes, dia);
     }
 
+    /**
+     * Método para obtener el NIF de la persona.
+     *
+     * @return El NIF de la persona.
+     */
     public Nif getNif() {
         return nif;
     }
 
+    /**
+     * Método para establecer el NIF de la persona.
+     *
+     * @param nif El NIF de la persona a establecer.
+     */
     public void setNif(Nif nif) {
         this.nif = nif;
     }
 
+    /**
+     * Método para obtener el nombre de la persona.
+     *
+     * @return El nombre de la persona.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Método para establecer el nombre de la persona.
+     *
+     * @param nombre El nombre de la persona a establecer.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Método para obtener el género de la persona.
+     *
+     * @return El género de la persona.
+     */
     public char getGenero() {
         return genero;
     }
 
+    /**
+     * Método para establecer el género de la persona.
+     *
+     * @param genero El género de la persona a establecer.
+     */
     public void setGenero(char genero) {
         this.genero = genero;
     }
 
+    /**
+     * Método para obtener la fecha de nacimiento de la persona.
+     *
+     * @return La fecha de nacimiento de la persona.
+     */
     public LocalDate getNacimiento() {
         return nacimiento;
     }
 
+    /**
+     * Método para establecer la fecha de nacimiento de la persona.
+     *
+     * @param nacimiento La fecha de nacimiento de la persona a establecer.
+     */
     public void setNacimiento(LocalDate nacimiento) {
         this.nacimiento = nacimiento;
     }
 
+    /**
+     * Método para obtener la edad de la persona.
+     *
+     * @return La edad de la persona.
+     */
     public int getEdad() {
         return Period.between(nacimiento, LocalDate.now()).getYears();
     }
 
+    /**
+     * Método que genera una representación en forma de cadena de texto de la
+     * persona.
+     *
+     * @return Una cadena de texto que representa a la persona con su NIF,
+     * nombre y edad.
+     */
     @Override
     public String toString() {
         if (nombre.split(" ").length > 1) {
@@ -82,6 +156,12 @@ public class Persona implements Comparable<Persona> {
         }
     }
 
+    /**
+     * Método para comparar si dos personas son iguales.
+     *
+     * @param a La persona a comparar.
+     * @return true si las personas tienen el mismo NIF, false de lo contrario.
+     */
     public boolean equals(Persona a) {
         if (a == null) {
             return false;
@@ -89,6 +169,12 @@ public class Persona implements Comparable<Persona> {
         return a.nif.toString().equals(this.nif.toString());
     }
 
+    /**
+     * Método para comparar si dos objetos Persona son iguales.
+     *
+     * @param obj El objeto a comparar.
+     * @return true si los objetos tienen el mismo NIF, false de lo contrario.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -105,6 +191,14 @@ public class Persona implements Comparable<Persona> {
         return Objects.equals(this.nif, other.nif);
     }
 
+    /**
+     * Método para comparar dos personas basado en su NIF.
+     *
+     * @param o La persona a comparar.
+     * @return Un valor negativo si el NIF de esta persona es menor que el de la
+     * otra, cero si son iguales, o un valor positivo si el NIF de esta persona
+     * es mayor que el de la otra.
+     */
     @Override
     public int compareTo(Persona o) {
         return this.nif.toString().compareTo(o.nif.toString());
